6edb12c496a0a1e2b0f8d349099b132ee792c257
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-11 17:05:13 -0500

    Added gateway features

--- src/main/java/com/avantica/todozuulgateway/TodoZuulGatewayApplication.java
+++ src/main/java/com/avantica/todozuulgateway/TodoZuulGatewayApplication.java
@@ -2,8 +2,12 @@ package com.avantica.todozuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient 		// It acts as a eureka client
+@EnableZuulProxy		// Enable Zuul
 public class TodoZuulGatewayApplication {
 
 	public static void main(String[] args) {
