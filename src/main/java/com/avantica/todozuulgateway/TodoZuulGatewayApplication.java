2ca55ee2c2f509bfe25bac33557205eaf9b664f0
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-11 13:21:14 -0500

    First commit

---
+++ src/main/java/com/avantica/todozuulgateway/TodoZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todozuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoZuulGatewayApplication.class, args);
+	}
+
+}
