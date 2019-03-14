efb868ad248d4ff58db831144d57dab2c8959c8a
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-14 00:54:49 -0500

    Cleaning up code

--- src/main/java/com/avantica/zuul/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/avantica/zuul/security/JwtAuthenticationEntryPoint.java
@@ -15,8 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED");
         
-        // How to customize the retured message?
-        // Link: https://stackoverflow.com/a/40791087
         String json = String.format("{\"message\": \"%s\"}", e.getMessage());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
--- src/main/java/com/avantica/zuul/security/SecurityTokenConfig.java
+++ src/main/java/com/avantica/zuul/security/SecurityTokenConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity 	// Enable security config. This annotation denotes config for spring security.
+@EnableWebSecurity
 public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtConfig jwtConfig;
@@ -20,21 +20,14 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
   	protected void configure(HttpSecurity http) throws Exception {
     	   http
 		.csrf().disable()
-		    // make sure we use stateless session; session won't be used to store user's state.
-	 	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
+		    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
 		.and()
-		    // handle an authorized attempts 
 		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)) 	
 		.and()
-		   // Add a filter to validate the tokens with every request
 		   .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-		// authorization requests config
 		.authorizeRequests()
-		   // allow all who are accessing "auth" service
 		   .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()  
-		   // must be an admin if trying to access admin area (authentication is also required here)
-		   .antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
-		   // Any other request must be authenticated
+		   .antMatchers("/reminder" + "/admin/**").hasRole("ADMIN")
 		   .anyRequest().authenticated(); 
 	}
 	
